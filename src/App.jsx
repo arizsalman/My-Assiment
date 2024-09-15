09f1b32ed9125b011a107dade9decd7d8b336a39
arizsalman <muhammadarizsalman@gmail.com>
2024-09-15 23:24:36 +0500

    done

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 import './App.css'
 
 import Header from './components/Header';
-import ProductList from './components/card';
+import ProductList from './components/Card';
 
 
 function App() {
